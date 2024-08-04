c22f6746a5fcf5a3c19047f1eaeb8e32dab56c45
Dhaval <kdkothari7@gmail.com>
2024-08-04 17:23:23 +0530

    order list table made responsive

--- app/orderList/page.jsx
+++ app/orderList/page.jsx
@@ -8,11 +8,11 @@ export default async function OrderPage() {
   const orders = await getOrderList();
 
   return (
-    <div>
+    <div className="w-full">
       <div>
         <HeaderGuj />
       </div>
-      <div className="w-full mt-5">
+      <div className="w-full mt-5 overflow-scroll md:text-base text-sm ">
         <table className="w-[90%] mx-auto border-[#262626] border-2">
           <thead className="border font-bold">
             <tr className="border">
@@ -32,7 +32,7 @@ export default async function OrderPage() {
                 <td className="border">{order.clientMoNum}</td>
                 <td className="border">{order.orderDate}</td>
                 <td className="border">{order.eventAddress}</td>
-                <td className="flex gap-2 items-center justify-center">
+                <td className="flex md:flex-row flex-col gap-2 items-center justify-center m-1">
                   <Link
                     href={`/orderList/${order._id}`}
                     className="bg-blue-500 rounded-md px-2 py-1"
