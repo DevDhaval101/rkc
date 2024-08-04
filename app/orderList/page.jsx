4e892105893489d1a004e3746f4000c28da00b95
Dhaval <kdkothari7@gmail.com>
2024-08-04 14:24:46 +0530

    serial no. added in order list page

--- app/orderList/page.jsx
+++ app/orderList/page.jsx
@@ -2,42 +2,50 @@ import getOrderList from "@/app/utils/getOrderList";
 import Link from "next/link";
 
 import DeleteBtn from "@/app/ui/deleteBtn";
+import HeaderGuj from '@/app/components/headerGuj'
 
 export default async function OrderPage() {
   const orders = await getOrderList();
 
   return (
-    <div className="w-full">
-      <table className="w-[90%] mx-auto border-[#262626] border-2">
-        <thead className="border font-bold">
-          <tr className="border">
-            <th className="border">નામ</th>
-            <th className="border">મોબાઇલ નંબર</th>
-            <th className="border">ઓર્ડર તારીખ</th>
-            <th className="border">પ્રસંગનુ સરનામુ</th>
-            <th className="border">CTA</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr className="border" key={order._id}>
-              <td className="border">{order.clientName}</td>
-              <td className="border">{order.clientMoNum}</td>
-              <td className="border">{order.orderDate}</td>
-              <td className="border">{order.eventAddress}</td>
-              <td className="flex gap-2 items-center justify-center">
-                <Link
-                  href={`/orderList/${order._id}`}
-                  className="bg-blue-500 rounded-md px-2 py-1"
-                >
-                  View
-                </Link>
-                <DeleteBtn orderId={String(order._id)}/>
-              </td>
+    <div>
+      <div>
+        <HeaderGuj />
+      </div>
+      <div className="w-full mt-5">
+        <table className="w-[90%] mx-auto border-[#262626] border-2">
+          <thead className="border font-bold">
+            <tr className="border">
+            <th className="border">અનુક્રમ નંબર</th>
+              <th className="border">નામ</th>
+              <th className="border">મોબાઇલ નંબર</th>
+              <th className="border">ઓર્ડર તારીખ</th>
+              <th className="border">પ્રસંગનુ સરનામુ</th>
+              <th className="border">CTA</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr className="border" key={order._id}>
+                <td className="border text-center">{index + 1}</td>
+                <td className="border">{order.clientName}</td>
+                <td className="border">{order.clientMoNum}</td>
+                <td className="border">{order.orderDate}</td>
+                <td className="border">{order.eventAddress}</td>
+                <td className="flex gap-2 items-center justify-center">
+                  <Link
+                    href={`/orderList/${order._id}`}
+                    className="bg-blue-500 rounded-md px-2 py-1"
+                  >
+                    View
+                  </Link>
+                  <DeleteBtn orderId={String(order._id)} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
